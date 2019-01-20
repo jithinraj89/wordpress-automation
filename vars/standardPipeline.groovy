b25b141856218e27c0bf33c7292dc9c6b803f0b1
jithin raj <jithin.raj@auto-grid.com>
2019-01-20 21:36:46 +0530

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -1,7 +1,7 @@
 def call(body) {
 
         def config = [:]
-        def PLAYBOOK_PATH = "/var/lib/jenkins/workspace/Jenkins_Deployments"
+        def PLAYBOOK_PATH = "/var/lib/jenkins/workspace"
         body.resolveStrategy = Closure.DELEGATE_FIRST
         body.delegate = config
         
@@ -27,10 +27,11 @@ def call(body) {
                         script {
                             if (params.REQUESTED_ACTION == 'rabbitmq'){
                                 echo 'Depoying rabbitmq '
-                                sh "cd ${PLAYBOOK_PATH}/${config.jobName}/ansible/playbooks/${config.folderName} && sudo cp ~/ansible.cfg ansible.cfg && sudo ansible-playbook -i ${PLAYBOOK_PATH}/${config.jobName}/ansible/playbooks/${config.folderName}/inventory/${config.inventoryFolder}/${config.envName} ${PLAYBOOK_PATH}/${config.jobName}/ansible/playbooks/${config.folderName}/rabbitmq.yml --tags ${config.tagName} --vault-password-file  ~/.agv"
+                                sh "cd ${PLAYBOOK_PATH}/${config.jobName} && sudo cat README.md"
                             }
                         }
                     }
                 }
+            }
 
 
